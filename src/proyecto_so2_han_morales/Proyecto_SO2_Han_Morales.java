334e455a52fc9ff6df5179aeded3f88c8271f6de
Erika Han <e.han@correo.unimet.edu.ve>
2020-07-12 19:23:58 -0400

    first commit

---
+++ src/proyecto_so2_han_morales/Proyecto_SO2_Han_Morales.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto_so2_han_morales;
+
+/**
+ *
+ * @author EH
+ */
+public class Proyecto_SO2_Han_Morales {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
